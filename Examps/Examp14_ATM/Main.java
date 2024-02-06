d21ef0c2453a1814795fe44dee96264baa756c1a
Batuhan KANBER <104573473+BatuhanKANBER@users.noreply.github.com>
2024-02-07 02:04:22 +0300

    Update Main.java

--- Examps/Examp14_ATM/Main.java
+++ Examps/Examp14_ATM/Main.java
@@ -2,13 +2,9 @@ package Examps.Examp14_ATM;
 
 public class Main {
     public static void main(String[] args) {
-
         Account account = new Account("batuhan", "123", 1000);
-
+        
         ATM atm = new ATM();
-
         atm.execute(account);
-
-
     }
 }
