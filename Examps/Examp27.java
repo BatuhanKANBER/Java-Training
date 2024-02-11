091d4993cab3a827ccfd54449d299dc391aa64ec
Batuhan KANBER <batuhankanber28@gmail.com>
2024-02-11 05:27:14 +0300

    Anagram.

---
+++ Examps/Examp27.java
@@ -0,0 +1,52 @@
+package Examps;
+//Anagram
+public class Examp27 {
+
+    public static boolean isAnagram(String s1, String s2){
+        int count = 0;
+
+        int len1 = s1.length();
+        int len2 = s2.length();
+
+        char[] chArr1 = s1.toCharArray();
+        char[] chArr2 = s2.toCharArray();
+
+        if (len1 != len2) {
+            return false;
+        }
+
+        len2 -= 1;
+        for (int i = 0; i < len1; i++){
+            if (chArr1[i] == chArr2[len2]){
+                count++;
+            }
+            len2--;
+        }
+
+        if (count == len1)
+            return true;
+
+        return false;
+    }
+
+    public static String anagram(String s1){
+        StringBuilder sAnagram = new StringBuilder();
+        int len = s1.length();
+        char[] chArr = s1.toCharArray();
+
+        for (int i = len - 1; i >= 0; i--){
+            sAnagram.append(chArr[i]);
+        }
+
+        return sAnagram.toString();
+    }
+    public static void main(String[] args) {
+        String s1 = "batuhan";
+        System.out.println(s1 + " kelimesinin anagramı: " + anagram(s1));
+        String s2 = anagram(s1);
+
+        if (isAnagram(s1, s2)){
+            System.out.println(s2 + " kelimesi " + s1 + " kelimesinin anagramı.");
+        }
+    }
+}
