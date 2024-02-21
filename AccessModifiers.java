ec38e4941cf622884ff4898b11f6cc618593f44b
Batuhan KANBER <batuhankanber28@gmail.com>
2024-02-21 15:12:24 +0300

    Access Modifiers.

---
+++ AccessModifiers.java
@@ -0,0 +1,22 @@
+public class AccessModifiers {
+    /*Sınıf Düzeyinde
+    *
+    * default => kendi paket içinden erişilebilir.
+    *
+    * public => kendi paketinden ve dışarıdaki paketlerden erişilebilir.
+    *
+    */
+
+    /*Metodlar ve Özellik Düzeyinde
+    *
+    * default => sadece kendi paketinin içinde bulunan classlar tarafından erişileblir.
+    *
+    * public => kendi paketi içinden ve dışındaki classların hepsinden erişilebilir.
+    *
+    * private => kendi classı içerisinde erişilebilir.
+    *
+    * protacted => kendi paketinin içerisindeki classlardan erişilebilir. Farklı pakettende bulunduğu sınıfın alt sınıfından erişilebilir.
+    *
+    */
+
+}
